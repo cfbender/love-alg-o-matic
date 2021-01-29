e73ee02152eba9940724d726d2a3bf442420c463
Monkey <munkeyxis@gmail.com>
2021-01-28 21:06:30 -0500

    InterestTemplate and Manager, starter Interests

---
+++ Assets/Scripts/Interests/InterestTemplate.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName = "Core/Interest")]
+public class InterestTemplate : ScriptableObject
+{
+    public string[] UniqueCallouts;
+}
--- Assets/Scripts/Managers.cs
+++ Assets/Scripts/Managers.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class Managers : MonoBehaviour
 {
     public static ProfileManager ProfileManager;
+    public static InterestManager InterestManager;
 
     private void Awake()
     {
         ProfileManager = GetComponent<ProfileManager>();
+        InterestManager = GetComponent<InterestManager>();
     }
 }
