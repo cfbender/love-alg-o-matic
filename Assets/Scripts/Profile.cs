2d197de331a9a5bbfe00e99138be26d3cfc33270
Monkey <munkeyxis@gmail.com>
2021-01-29 09:32:15 -0500

    Profile: add Guid Id, whitespace

--- Assets/Scripts/Profile.cs
+++ Assets/Scripts/Profile.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 public class Profile
 {
+    public Guid Id { get; }
     public string Name { get; }
     public List<InterestTemplate> Interests { get; }
 
-    private readonly List<string> Names = new List<string>() {"Jimmy Joey", "Tina Tuna", "Paulie Porkchop", "Chris Christie", "Lilly Lollipop", "Sean Swordfish"};
+    private readonly List<string> Names = new List<string>()
+        {"Jimmy Joey", "Tina Tuna", "Paulie Porkchop", "Chris Christie", "Lilly Lollipop", "Sean Swordfish"};
+
     public Profile()
     {
-       Interests = Managers.InterestManager.GetInterests();
-       Name = Names[Managers.ProfileManager.Random.Next(Names.Count)];
+        Id = Guid.NewGuid();
+        Interests = Managers.InterestManager.GetInterests();
+        Name = Names[Managers.ProfileManager.Random.Next(Names.Count)];
     }
 }
