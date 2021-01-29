d2dece39154c89a2a322ea63d930452c9b503b1d
cfbender <codyfbender@gmail.com>
2021-01-29 13:06:25 -0700

    feat: evaluation profile control

---
+++ Assets/Scripts/Evaluator/EvaluationProfileControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class EvaluationProfileControl : MonoBehaviour
+{
+    public Sprite profilePlaceholder;
+    public Sprite emptySprite;
+    public Image profileImage;
+    private TextMeshProUGUI _nameText;
+    private Profile _profile;
+
+    private void Start()
+    {
+        _nameText = GetComponentInChildren<TextMeshProUGUI>();
+        RemoveProfile();
+    }
+    
+    public void AssignProfile(Profile profile)
+    {
+        _profile = profile;
+        _nameText.text = profile.Name;
+        profileImage.sprite = profilePlaceholder;
+    }
+
+    public void RemoveProfile()
+    {
+        _profile = null;
+        _nameText.text = string.Empty;
+        profileImage.sprite = emptySprite;
+    }
+}
