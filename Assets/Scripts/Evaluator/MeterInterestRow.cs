e39ff9bdf93eb7e472fd7dec5536cf4c5915f1ff
Monkey <munkeyxis@gmail.com>
2021-01-30 10:53:58 -0500

    love meter ui, prefab font updates

---
+++ Assets/Scripts/Evaluator/MeterInterestRow.cs
@@ -0,0 +1,10 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeterInterestRow : MonoBehaviour
+{
+    public TextMeshProUGUI Profile1InterestTextObject;
+    public Image HeartImage;
+    public TextMeshProUGUI Profile2InterestTextObject;
+}
