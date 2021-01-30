4eff9b8868b169b94cf4860d99d8a59a71150481
cfbender <codyfbender@gmail.com>
2021-01-30 15:15:46 -0700

    chore: stub score screen method

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,27 +6,27 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI ScoreText;
-    
+
     public List<MatchedProfile> matches = new List<MatchedProfile>();
     public List<MatchedProfile> failures = new List<MatchedProfile>();
     public int matchCount;
     public int failureCount;
     public int maxFailures = 3;
     public int maxMatches = 10;
-    
+
     public int currentStreak;
     public bool currentStreakSuccessful;
     public int longestSuccessStreak;
     public int longestMissedStreak;
-    
-    
+
+
     public void AddMatchedPair(Profile profile1, Profile profile2, bool failure)
     {
         var list = failure ? failures : matches;
-       list.Add(new MatchedProfile(profile1, profile2)); 
-       matchCount = matches.Count;
-       failureCount = failures.Count;
-       GenerateScoreText();
+        list.Add(new MatchedProfile(profile1, profile2));
+        matchCount = matches.Count;
+        failureCount = failures.Count;
+        GenerateScoreText();
     }
 
     public void UpdateStreaks(bool success)
@@ -51,10 +51,14 @@ public class ScoreManager : MonoBehaviour
         }
     }
 
+    public void ShowFinalScoreScreen()
+    {
+        Debug.Log("Final score screen activated.");
+    }
+
     private void GenerateScoreText()
     {
         ScoreText.text = $"Successful Matches: {matchCount} \n" +
                          $"Unsuccessful Matches: {failureCount}";
     }
-    
 }
