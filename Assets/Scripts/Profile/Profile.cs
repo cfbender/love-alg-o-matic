4e3df70f265b92417b58311fbece4ae9ce67bb19
Brian McLendon <mclendon.brian@gmail.com>
2021-01-29 23:58:03 -0500

    Anti Kissing Cousins & Verbose Naming

--- Assets/Scripts/Profile/Profile.cs
+++ Assets/Scripts/Profile/Profile.cs
@@ -7,9 +7,6 @@ public class Profile
     public string Name { get; }
     public List<InterestTemplate> Interests { get; }
 
-    private readonly List<string> Names = new List<string>()
-        {"Jimmy Joey", "Tina Tuna", "Paulie Porkchop", "Chris Christie", "Lilly Lollipop", "Sean Swordfish"};
-
     public Profile()
     {
         Id = Guid.NewGuid();
